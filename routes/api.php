213f78c26b992e3a9dff6566980d8329a957e202
vitrasethy <sethy.vitra@gmail.com>
2024-11-01 13:54:14 +0700

    No error.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\FieldController;
+use App\Http\Controllers\SportTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('fields', FieldController::class);
+Route::apiResource('sport-type', SportTypeController::class);
