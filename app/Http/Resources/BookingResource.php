f7e9b0c82069e64c961c6d28bae09f71ea7ac8e3
vitrasethy <sethy.vitra@gmail.com>
2025-01-24 09:07:24 +0700

    hehe

--- app/Http/Resources/BookingResource.php
+++ app/Http/Resources/BookingResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use App\Models\Booking;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 /** @mixin Booking */
 class BookingResource extends JsonResource
@@ -13,8 +14,8 @@ class BookingResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'start_date' => $this->start_date,
-            'end_date' => $this->end_date,
+            'start_date' => Carbon::parse($this->start_date)->toDayDateTimeString(),
+            'end_date' => Carbon::parse($this->end_date)->toDayDateTimeString(),
 
             'field' => new FieldResource($this->whenLoaded('field')),
         ];
